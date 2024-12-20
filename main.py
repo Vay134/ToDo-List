7bc989580738ba19e98b0b5748a864df0ed642d3
Vay134 <71159266+Vay134@users.noreply.github.com>
2024-12-20 17:41:08 +0800

    Worked on userinput

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import os
 
 class main_app():
     def __init__(self):
-        self.print_info()
         self.get_data()
-        self.print_tasks(self.unfinished)
-        self.print_all_tasks()
+        self.print_info()
 
     def print_info(self):
         print("==================================================")
@@ -14,7 +12,7 @@ class main_app():
         return
 
     def get_data(self):
-        if not os.path.exists("save_data.txt"): # Createsa new save file if it does not exist
+        if not os.path.exists("save_data.txt"): # Creates a new save file if it does not exist
             with open("save_data.txt", "w+") as f:
                 f.write("0\n") # Max Task ID
                 f.write("COMPLETED: \n")
@@ -30,24 +28,25 @@ class main_app():
         return
 
     def print_tasks(self, task_type: list):
+        print("--------------------------------------------------\n")
         print("                 ", end = "")
         print("Unfinished Tasks" if task_type == self.unfinished else "Completed Tasks", end = "")
         print("                  ")
-        print("--------------------------------------------------")
         print("[U]: Unfinished")
         print("[C]: Completed")
-        print("The number to the the left of each task it its ID\n")
+        print("The number to the the left of each task it its ID")
+        print("--------------------------------------------------")
 
         for task_ID in task_type:
             print(task_ID, self.id_task[task_ID])
         
-        print("--------------------------------------------------\n")
+        print("--------------------------------------------------")
     
     def print_all_tasks(self):
+        print("--------------------------------------------------\n")
         print("                    All Tasks                     ")
         print("[U]: Unfinished")
         print("[C]: Completed")
-
         print("The number to the the left of each task it its ID")
         print("--------------------------------------------------")
 
@@ -57,17 +56,54 @@ class main_app():
         for completed_task_ID in self.completed:
             print("[C]", completed_task_ID, self.id_task[completed_task_ID])
         
-        print("--------------------------------------------------\n")
+        print("--------------------------------------------------")
     
+    def view_tasks(self):
+        task_view_mapper = {"c": (lambda: self.print_tasks(self.completed)), 
+                            "u": (lambda:self.print_tasks(self.unfinished)), 
+                            "a": self.print_all_tasks}
+        print("--------------------------------------------------")
+        print("All tasks: a")
+        print("Unfinished tasks: u")
+        print("Completed tasks: c")
+        print("Back: b")
+        inp = input("Which tasks would you like to view? ")
+        while inp.lower() not in task_view_mapper:
+            inp = input("Invalid input, please enter a/u/c/b: ")
+        if inp.lower() == 'b':
+            return
+        task_view_mapper[inp]()
+        return
+
+    def update_task(self):
+        print("B")
+        pass
+
+    def edit_task(self):
+        print("B")
+        pass
+
+    def delete_task(self):
+        print("B")
+        pass
+
     def get_action(self):
+        actions = {"v": self.view_tasks, "u": self.update_task, "e": self.edit_task, "d": self.delete_task}
         print("View tasks: v")
         print("Update a task: u")
         print("Edit a task: e")
         print("Delete a task: d")
+        print("Exit: x")
+        print("--------------------------------------------------")
         inp = input("What would you like to do? ")
-        if inp.lower() not in ('v', 'u', 'e', 'd'):
+        if inp.lower() == 'x':
+            return True
+        while inp.lower() not in actions:
             inp = input("Invalid input, please enter v/u/e/d: ")
-        return inp
-        
+        actions[inp]()
+        return False
 
 app = main_app()
+end_program = False
+while end_program == False:
+    end_program = app.get_action()
